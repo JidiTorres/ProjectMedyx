627d5cd4d09d9c977b45e96b9a7fdd9c3912e4d2
Jean-Dominique Quintal-Torres <jean-dominique.quintal-torres.1@etsmtl.net>
2022-04-06 09:20:02 -0400

    fini

--- medyx-app2/src/App.js
+++ medyx-app2/src/App.js
@@ -1,27 +1,22 @@
 import {Route, Routes} from 'react-router-dom'
 
-import AllMeetUpsPage from './pages/AllMeetups';
+import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 
 function App() {
-  return <div>
-    <MainNavigation>
-      <Routes>
-        <Route path='/' exact>
-          <AllMeetUpsPage />
-        </Route>
-        <Route path='/new-meetup'>
-          <NewMeetupPage />
-        </Route>
-        <Route path='/favorites'>
-          <FavoritesPage />
-        </Route>
-      </Routes>
-    </MainNavigation>
-  </div>;
+  return (
+    <Layout>
+      
+        <Routes>
+          <Route path="/" element={<AllMeetupsPage />} />
+          <Route path="/new-meetup" element={<NewMeetupPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
